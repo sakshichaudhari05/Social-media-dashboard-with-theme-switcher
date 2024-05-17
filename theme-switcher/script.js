83e66c3c54e52fbb87e3d8962ed54da359da457b
Sakshi Dinkar Chaudhari <91466548+sakshichaudhari05@users.noreply.github.com>
2024-05-18 00:29:20 +0530

    Add files via upload

---
+++ theme-switcher/script.js
@@ -0,0 +1,10 @@
+// const themeToggle = document.getElementById('theme-toggle');
+// const body = document.body;
+
+// themeToggle.addEventListener('change', () => {
+//     body.classList.toggle('dark-mode');
+// });
+
+document.getElementById('theme-toggle').addEventListener('change', function() {
+    document.body.classList.toggle('dark-theme');
+});
